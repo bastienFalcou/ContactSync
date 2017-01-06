08a443f5ab438b101eb6bbe55f4d65679b4782a4
Bastien Falcou <bastien.falcou@hotmail.com>
2017-01-06 10:25:22 +0000

    chore(Project): create template xcode project

---
+++ ContactSyncing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactSyncing
+//
+//  Created by Bastien Falcou on 1/6/17.
+//  Copyright © 2017 Bastien Falcou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
